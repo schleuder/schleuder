72e49752cf18920bc5bec4e03631fff4f565b3b7
paz <paz@nadir.org>
2014-07-17 09:17:47 +0200

    Attach original message to admin-email.

--- lib/schleuder/logger_notifications.rb
+++ lib/schleuder/logger_notifications.rb
@@ -10,9 +10,8 @@ module Schleuder
     end
 
     def fatal(string, original_message=nil)
-      string  = string.to_s << append_original_message(original_message)
-      super(string)
-      notify_admin(string)
+      super(string.to_s + append_original_message(original_message))
+      notify_admin(string, original_message)
     end
 
     private
@@ -25,13 +24,19 @@ module Schleuder
       end
     end
 
-    def notify_admin(string)
+    def notify_admin(string, original_message=nil)
       Array(adminaddresses).each do |address|
         mail = Mail.new
         mail.from = @from
         mail.to = address
         mail.subject = 'Error'
-        mail.body = string.to_s
+        msgpart = Mail::Part.new
+        msgpart.body = string.to_s
+        mail.add_part msgpart
+        if original_message
+          mail.add_part Mail::Part.new(original_message)
+          mail.parts.last.content_description = 'The originally incoming message'
+        end
         mail.deliver
       end
     end
