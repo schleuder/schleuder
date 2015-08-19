c55c6b2aacf30a1e422fd764416d3d083045ec8f
paz <paz@nadir.org>
2015-08-19 18:28:05 +0200

    Rotate list's logfiles every 100KB and keep one old file.

--- lib/schleuder/listlogger.rb
+++ lib/schleuder/listlogger.rb
@@ -2,7 +2,7 @@ module Schleuder
   class Listlogger < ::Logger
     include LoggerNotifications
     def initialize(filename, list)
-      super(filename)
+      super(filename, 2, 102400)
       @from = list.email
       @adminaddresses = list.admins.map(&:email)
       @level = ::Logger.const_get(list.log_level.upcase)
