1b80c598ffe0cb8d22266adeead579cd246f4eba
paz <paz@nadir.org>
2015-12-09 02:12:53 +0100

    Fix `install`-command.

--- lib/schleuder/cli.rb
+++ lib/schleuder/cli.rb
@@ -47,51 +47,55 @@ module Schleuder
       end
     end
 
-    desc 'install', "Set up Schleuder initially. Create folders, copy files, fill the database, etc."
+    method_option :schleuderpath, banner: '/path/to/code', desc: 'Path to code of schleuder (if not installed from gem)'
+    desc 'install [ --schleuderpath=/path/to/code ]', "Set up Schleuder initially. Create folders, copy files, fill the database."
     def install
       if `id -u`.to_i > 0
         fatal "root-privileges required"
       end
 
-      if ARGV.first == '--gem'
+      if options[:schleuderpath]
+        if ! File.directory?(options[:schleuderpath])
+          fatal "No such directory '#{options[:schleuderpath]}'"
+        end
+        path = options[:schleuderpath]
+      else
         spec = Gem::Specification.find_by_name('schleuder')
         path = spec.gem_dir
-      else
-        if ! File.directory?(ARGV.first)
-          fatal "No such directory '#{ARGV.first}'"
-        end
-        path = ARGV.first
       end
 
-      if ARGV.last != '--step2'
-        FileUtils.mkdir_p %w[/var/schleuder /var/schleuder/lists /etc/schleuder]
-        Dir[File.join(path, "etc/*")].each do |file|
-          target = "/etc/schleuder/#{File.basename(file)}"
-          if ! File.exists?(target)
-            FileUtils.cp file, target
-          end
+      FileUtils.mkdir_p %w[/var/schleuder /var/schleuder/lists /etc/schleuder]
+      Dir[File.join(path, "etc/*")].each do |file|
+        target = "/etc/schleuder/#{File.basename(file)}"
+        if ! File.exists?(target)
+          FileUtils.cp file, target
         end
-        say "Please adapt /etc/schleuder/schleuder.yml to your needs. If you choose a different DBMS than sqlite (e.g. mysql, postgresql) don't forget to create the database you configured and install the ruby-bindings (e.g. `gem install mysql`). Then run step 2: `#{$0} #{ARGV.join(' ')} --step2`"
-
-      else
-
-        # Step 2
-        say "Step 2: Preparing the database."
-        answer = ask "Ready? [yN] "
-        if answer.downcase != 'y'
-          say "Cancelled."
-          exit
-        end
-
-        say `cd #{path} && rake db:schema:load`
-
-        say "
-        Schleuder has been set up. You can now create a new list using schleuder-newlist.
-        We hope you enjoy!"
       end
+      say "Now please adapt /etc/schleuder/schleuder.yml to your needs. If you choose a different DBMS than sqlite (e.g. mysql, postgresql) don't forget to create the database you configured and install the ruby-bindings (e.g. `gem install mysql`)."
+
+      # TODO: test if database exists and is prepared already.
+      # Step 2
+      say "Next: Filling the database."
+      answer = ask "Ready? [yN] "
+      if answer.downcase != 'y'
+        say "Cancelled."
+        exit
+      end
+
+      say `cd #{path} && rake db:schema:load`
+
+      say "
+      Schleuder has been set up. You can now create a new list using schleuder-conf.
+      We hope you enjoy!"
     rescue => exc
       fatal exc.message
     end
 
+    no_commands do 
+      def fatal(msg)
+        error(msg)
+        exit 1
+      end
+    end
   end
 end
