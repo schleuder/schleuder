3968997c6d0186d5c385714f270dedd9d0fd0fac
Nina Siessegger <n.siessegger@zeromail.org>
2016-11-17 18:36:30 +0100

    Fix indentation

--- lib/schleuder/list.rb
+++ lib/schleuder/list.rb
@@ -283,19 +283,19 @@ module Schleuder
       end
     end
 
-      def delete_listdir
-        if File.exists?(self.listdir)
-          FileUtils.rm_r(self.listdir, secure: true)
-          Schleuder.logger.info "Deleted listdir"
-        else
-          # Don't use list-logger here — if the list-dir isn't present we can't log to it!
-          Schleuder.logger.info "Couldn't delete listdir, directly not present"
-        end
-        true
-      rescue => exc
+    def delete_listdir
+      if File.exists?(self.listdir)
+        FileUtils.rm_r(self.listdir, secure: true)
+        Schleuder.logger.info "Deleted listdir"
+      else
         # Don't use list-logger here — if the list-dir isn't present we can't log to it!
-        Schleuder.logger.error "Error while deleting listdir: #{exc}"
-        return false
+        Schleuder.logger.info "Couldn't delete listdir, directly not present"
       end
+      true
+    rescue => exc
+      # Don't use list-logger here — if the list-dir isn't present we can't log to it!
+      Schleuder.logger.error "Error while deleting listdir: #{exc}"
+      return false
+    end
   end
 end
