3ba5ca114fd53ef78a324fe724a0400943a1c034
paz <paz@nadir.org>
2016-11-23 00:10:38 +0100

    Fix creating lists.
    The value can be nil, sometimes.

--- lib/schleuder/validators/no_line_breaks_validator.rb
+++ lib/schleuder/validators/no_line_breaks_validator.rb
@@ -1,6 +1,6 @@
 class NoLineBreaksValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    if value.include?("\n")
+    if value.to_s.include?("\n")
       record.errors.add(attribute, I18n.t("errors.no_linebreaks") )
     end
   end
