e4da557e794a68f7d4cf30bcea1f63f23fbeb796
Nina Siessegger <n.siessegger@zeromail.org>
2016-12-08 11:42:46 +0100

    Use factory in subscription spec

--- spec/schleuder/subscription_spec.rb
+++ spec/schleuder/subscription_spec.rb
@@ -7,10 +7,6 @@ describe Schleuder::Subscription do
       :admin
   ].freeze
 
-  before(:example) do
-    @list = Schleuder::List.first || Schleuder::List.create(email: 'test@whatever', fingerprint: 'aaaadddd0000999')
-  end
-
   it "has a valid factory" do
     subscription = create(:subscription)
 
@@ -24,43 +20,28 @@ describe Schleuder::Subscription do
   it { is_expected.to respond_to :delivery_enabled }
 
   it "is invalid when list_id is blank" do
-    subscription = Schleuder::Subscription.new(
-      email: "foo@bar.org",
-      fingerprint: "aaaadddd0000999",
-    )
+    subscription = build(:subscription, list_id: "")
 
     expect(subscription).not_to be_valid
     expect(subscription.errors.messages[:list_id]).to be_present
   end
 
   it "is invalid when email is nil" do
-    subscription = Schleuder::Subscription.new(
-      list_id: @list.id,
-      email: nil,
-      fingerprint: "aaaadddd0000999",
-    )
+    subscription = build(:subscription, email: nil)
 
     expect(subscription).not_to be_valid
     expect(subscription.errors.messages[:email]).to include("can't be blank")
   end
 
    it "is invalid when email is blank" do
-    subscription = Schleuder::Subscription.new(
-      list_id: @list.id,
-      email: "",
-      fingerprint: "aaaadddd0000999",
-    )
+    subscription = build(:subscription, email: "")
 
     expect(subscription).not_to be_valid
     expect(subscription.errors.messages[:email]).to include("can't be blank")
   end
 
   it "is invalid when email does not contain an @" do
-    subscription = Schleuder::Subscription.new(
-      list_id: @list.id,
-      email: "fooatbar.org",
-      fingerprint: "aaaadddd0000999",
-    )
+    subscription = build(:subscription, email: "fooatbar.org")
 
     expect(subscription).not_to be_valid
     expect(subscription.errors.messages[:email]).to include("is not a valid email address")
@@ -68,39 +49,27 @@ describe Schleuder::Subscription do
 
   it "normalizes the fingerprint" do
     fingerprint = " 99 991 1000 10"
-    subscription = Schleuder::Subscription.new(fingerprint: fingerprint)
+    subscription = build(:subscription, fingerprint: fingerprint)
 
     expect(subscription.fingerprint).to eq "99991100010"
   end
 
   it "is valid when fingerprint is empty" do
-    subscription = Schleuder::Subscription.new(
-      list_id: @list.id,
-      email: "foo@bar.org",
-      fingerprint: "",
-    )
+    subscription = build(:subscription, fingerprint: "")
 
     expect(subscription).to be_valid
     expect(subscription.errors.messages[:fingerprint]).to be_blank
   end
 
   it "is valid when fingerprint is nil" do
-    subscription = Schleuder::Subscription.new(
-      list_id: @list.id,
-      email: "foo@bar.org",
-      fingerprint: nil
-    )
+    subscription = build(:subscription, fingerprint: nil)
 
     expect(subscription).to be_valid
     expect(subscription.errors.messages[:fingerprint]).to be_blank
   end
 
   it "is invalid when fingerprint contains invalid characters" do
-    subscription = Schleuder::Subscription.new(
-      list_id: @list.id,
-      email: "foo@bar.org",
-      fingerprint: "&$$$$123AAA",
-    )
+    subscription = build(:subscription, fingerprint: "&$$$$123AAA")
 
     expect(subscription).not_to be_valid
     expect(subscription.errors.messages[:fingerprint]).to include("is not a valid fingerprint")
@@ -108,11 +77,7 @@ describe Schleuder::Subscription do
 
   BOOLEAN_SUBSCRIPTION_ATTRIBUTES.each do |subscription_attribute|
     it "is invalid if #{subscription_attribute} is nil" do
-      subscription = Schleuder::Subscription.new(
-        list_id: @list.id,
-        email: "foo@bar.org",
-        fingerprint: nil,
-      )
+      subscription = build(:subscription)
       subscription[subscription_attribute] = nil
 
       expect(subscription).not_to be_valid
@@ -120,11 +85,7 @@ describe Schleuder::Subscription do
     end
 
     it "is invalid if #{subscription_attribute} is blank" do
-      subscription = Schleuder::Subscription.new(
-        list_id: @list.id,
-        email: "foo@bar.org",
-        fingerprint: nil,
-      )
+      subscription = build(:subscription)
       subscription[subscription_attribute] = ""
 
       expect(subscription).not_to be_valid
