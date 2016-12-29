ffd7c52ca385b27b704c15e9d14efa2e8ae40346
paz <paz@nadir.org>
2016-12-29 18:18:48 +0100

    Enable using the test-db from different processes.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,7 +19,7 @@ RSpec.configure do |config|
   end
 
   config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.strategy = :deletion
     DatabaseCleaner.clean_with(:truncation)
   end
 
