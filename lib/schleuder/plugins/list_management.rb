a209a3993b54a9e60b7687d3e425c9434495396e
paz <paz@nadir.org>
2017-02-16 13:06:31 +0100

    New: x-get-logfile
    Sends attached logfile of list.

---
+++ lib/schleuder/plugins/list_management.rb
@@ -0,0 +1,15 @@
+module Schleuder
+  module RequestPlugins
+    def self.get_logfile(arguments, list, mail)
+      if File.readable?(list.logfile)
+        attachment = Mail::Part.new
+        attachment.body = File.read(list.logfile)
+        attachment.content_disposition = "inline; filename=#{list.email}.log"
+        intro = I18n.t("plugins.list_management.logfile_attached", listname: list.email)
+        [intro, attachment]
+      else
+        I18n.t("plugins.list_management.no_logfile", listname: list.email)
+      end
+    end
+  end
+end
