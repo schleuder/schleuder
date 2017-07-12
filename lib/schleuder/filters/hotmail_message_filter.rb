826643a2ef2c4012e3e6f32082e2eaf048e899a7
ng <ng@immerda.ch>
2017-07-12 12:23:06 +0200

    document the intend of our fix
    in a few years someone might look at it and not understand what we
    are doing here. Let's give them some hints.

--- lib/schleuder/filters/hotmail_message_filter.rb
+++ lib/schleuder/filters/hotmail_message_filter.rb
@@ -1,6 +1,12 @@
 module Schleuder
   module Filters
 
+    # Outlook / Hotmail seems to dismantle multipart/encrypted messages and
+    # put them again together as multipart/mixed, which is wrong and makes
+    # it problematic to correctly detect the message as a valid pgp/mime-mail.
+    # Here we fix the mail to be a proper pgp/mime aka. multipart/encrypted
+    # message, so further processing will detect it properly.
+    # See #211 and #246 for background
     def self.fix_hotmail_messages!(list, mail)
       if mail.header['X-OriginatorOrg'].to_s.match(/(hotmail|outlook).com/) &&
           !mail[:content_type].blank? &&
