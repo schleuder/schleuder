f3cd6d4e7d7ba24b03ed8dc3141e8b9f53d01afa
paz <paz@nadir.org>
2018-02-03 12:52:19 +0100

    Use has_secure_password built-in for Account.

--- lib/schleuder/account.rb
+++ lib/schleuder/account.rb
@@ -1,5 +1,7 @@
 module Schleuder
   class Account < ActiveRecord::Base
+    has_secure_password
+
     has_many :subscriptions, foreign_key: "email", primary_key: "email"
     has_many :lists, through: :subscriptions
     has_many :admin_lists, through: :subscriptions
