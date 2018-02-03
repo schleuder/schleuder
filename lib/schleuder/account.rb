ba616934da24b73800e39c9bc0bc7d6fc0db28c9
paz <paz@nadir.org>
2018-02-03 11:41:49 +0100

    Fix association Account->Subscriptions

--- lib/schleuder/account.rb
+++ lib/schleuder/account.rb
@@ -1,6 +1,6 @@
 module Schleuder
   class Account < ActiveRecord::Base
-    has_many :subscriptions, foreign_key: "email"
+    has_many :subscriptions, foreign_key: "email", primary_key: "email"
     has_many :lists, through: :subscriptions
     has_many :admin_lists, through: :subscriptions
   end
