b59afc0e3873aa9f400f58735ac5b3d053dd56f9
ng <ng@immerda.ch>
2018-03-14 08:25:46 +0100

    test the whole corpus of thunderbird emails
    Quite a back ago we added corpus of emails from thunderbird, to
    ensure we are able to parse them. Though we never added the
    corpus to the tests.

--- spec/schleuder/integration/send_encrypted_spec.rb
+++ spec/schleuder/integration/send_encrypted_spec.rb
@@ -1,19 +1,26 @@
 require "spec_helper"
 
 describe "user sends an encrypted message" do
-  it "from thunderbird" do
-    start_smtp_daemon
-    list = create(:list)
-    list.subscribe("admin@example.org", nil, true)
-    message_path = 'spec/fixtures/mails/encrypted-mime/thunderbird.eml'
+  [
+    'encrypted-inline',
+    'encrypted-mime',
+    'encrypted+signed-inline',
+    'encrypted+signed-mime',
+  ].each do |t|
+    it "from thunderbird being #{t}" do
+      start_smtp_daemon
+      list = create(:list)
+      list.subscribe("admin@example.org", nil, true)
+      message_path = "spec/fixtures/mails/#{t}/thunderbird.eml"
 
-    error = run_schleuder(:work, list.email, message_path)
-    mails = Dir.glob("#{smtp_daemon_outputdir}/mail-*")
+      error = run_schleuder(:work, list.email, message_path)
+      mails = Dir.glob("#{smtp_daemon_outputdir}/mail-*")
 
-    expect(error).to be_empty
-    expect(mails.size).to eq 1
+      expect(error).to be_empty
+      expect(mails.size).to eq 1
 
-    stop_smtp_daemon
+      stop_smtp_daemon
+    end
   end
 end
 
