07cdf438d95ed0d63e33b392b996b2b19b03b231
paz <paz@nadir.org>
2018-06-23 18:23:08 +0200

    Make ApiDaemon-specs more clear.

--- spec/schleuder-api-daemon/requests/subscription_spec.rb
+++ spec/schleuder-api-daemon/requests/subscription_spec.rb
@@ -4,65 +4,89 @@ describe 'subscription via api' do
 
   before :each do
     @list = List.last || create(:list)
-    @email = create(:subscription).email
+    @email = 'someone@localhost'
   end
 
   it 'doesn\'t subscribe new member without authorization' do
     parameters = {'list_id' => @list.id, :email => @email}
-    expect {
-      post '/subscriptions.json', parameters.to_json
-      expect(last_response.status).to be 401
-    }.to change { Subscription.count }.by 0
+
+    expect(@list.subscriptions.size).to be(0)
+
+    post '/subscriptions.json', parameters.to_json
+
+    expect(last_response.status).to be 401
+    expect(@list.reload.subscriptions.size).to be(0)
   end
 
   it 'subscribes new member to a list' do
     authorize!
     parameters = {'list_id' => @list.id, :email => @email}
-    expect {
-      post '/subscriptions.json', parameters.to_json
-      expect(last_response.status).to be 201
-    }.to change { Subscription.count }.by 1
-    expect(Subscription.where(:email => @email).first.admin?).to be false
-    expect(Subscription.where(:email => @email).first.delivery_enabled).to be true
+
+    expect(@list.subscriptions.size).to be(0)
+
+    post '/subscriptions.json', parameters.to_json
+    @list.reload
+
+    expect(last_response.status).to be 201
+    expect(@list.subscriptions.map(&:email)).to eql([@email])
+    expect(@list.subscriptions.first.admin?).to be false
+    expect(@list.subscriptions.first.delivery_enabled).to be true
   end
 
   it 'subscribes an admin user' do
     authorize!
     parameters = {'list_id' => @list.id, :email => @email, :admin => true}
-    expect {
-      post '/subscriptions.json', parameters.to_json
-      expect(last_response.status).to be 201
-    }.to change { Subscription.count }.by 1
-    expect(Subscription.where(:email => @email).first.admin?).to be true
+
+    expect(@list.subscriptions.size).to be(0)
+
+    post '/subscriptions.json', parameters.to_json
+    @list.reload
+
+    expect(last_response.status).to be 201
+    expect(@list.subscriptions.map(&:email)).to eql([@email])
+    expect(@list.subscriptions.first.admin?).to be true
+    expect(@list.subscriptions.first.delivery_enabled).to be true
   end
 
   it 'subscribes an admin user with a truthy value' do
     authorize!
     parameters = {'list_id' => @list.id, :email => @email, :admin => '1'}
-    expect {
-      post '/subscriptions.json', parameters.to_json
-      expect(last_response.status).to be 201
-    }.to change { Subscription.count }.by 1
-    expect(Subscription.where(:email => @email).first.admin?).to be true
+
+    expect(@list.subscriptions.size).to be(0)
+
+    post '/subscriptions.json', parameters.to_json
+    @list.reload
+
+    expect(last_response.status).to be 201
+    expect(@list.subscriptions.map(&:email)).to eql([@email])
+    expect(@list.subscriptions.first.admin?).to be true
+    expect(@list.subscriptions.first.delivery_enabled).to be true
   end
 
   it 'subscribes an user and unsets delivery flag' do
     authorize!
     parameters = {'list_id' => @list.id, :email => @email, :delivery_enabled => false}
-    expect {
-      post '/subscriptions.json', parameters.to_json
-      expect(last_response.status).to be 201
-    }.to change { Subscription.count }.by 1
-    expect(Subscription.where(:email => @email).first.delivery_enabled).to be false
+
+    expect(@list.subscriptions.size).to be(0)
+
+    post '/subscriptions.json', parameters.to_json
+    @list.reload
+
+    expect(last_response.status).to be 201
+    expect(@list.subscriptions.map(&:email)).to eql([@email])
+    expect(@list.subscriptions.first.admin?).to be false
+    expect(@list.subscriptions.first.delivery_enabled).to be false
   end
 
   it 'unsubscribes members' do
-    authorize!
     subscription = create(:subscription, :list_id => @list.id)
-    parameters = {'list_id' => @list.id, :email => @email, :delivery_enabled => false}
-    expect {
-      delete "/subscriptions/#{subscription.id}.json"
-      expect(last_response.status).to be 200
-    }.to change { Subscription.count }.by -1
+    authorize!
+
+    expect(@list.subscriptions.map(&:email)).to eql([subscription.email])
+
+    delete "/subscriptions/#{subscription.id}.json"
+
+    expect(last_response.status).to be 200
+    expect(@list.reload.subscriptions.map(&:email)).to eql([])
   end
 end
