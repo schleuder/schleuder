d52d2ed6801ca2d9cb49a9f35c3c84721aff07ff
paz <paz@nadir.org>
2018-07-13 11:13:20 +0200

    API: Fix spec for /status and /version

--- spec/schleuder-api-daemon/requests/version_spec.rb
+++ spec/schleuder-api-daemon/requests/version_spec.rb
@@ -1,8 +1,16 @@
 require 'helpers/api_daemon_spec_helper'
 
 describe 'version' do
-  it 'returns the current schleuder version' do
+  it 'does not return the current schleuder version if not authorized' do
+    get '/version.json'
+
+    expect(last_response.status).to be 401
+    expect(last_response.body).not_to eq "{\"version\":\"#{Schleuder::VERSION}\"}"
+  end
+
+  it 'returns the current schleuder version if authorized' do
     authorize!
+
     get '/version.json'
 
     expect(last_response.status).to be 200
