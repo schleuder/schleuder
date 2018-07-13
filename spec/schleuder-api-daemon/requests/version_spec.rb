0c5cd2d7bebd30df809dfd5eca924cd8012ee0f9
paz <paz@nadir.org>
2018-07-13 11:13:20 +0200

    API: Fix spec for /status and /version

--- spec/schleuder-api-daemon/requests/version_spec.rb
+++ spec/schleuder-api-daemon/requests/version_spec.rb
@@ -1,8 +1,41 @@
 require 'helpers/api_daemon_spec_helper'
 
 describe 'version' do
-  it 'returns the current schleuder version' do
-    authorize!
+  it 'does not return the current schleuder version if not authorized' do
+    get '/version.json'
+
+    expect(last_response.status).to be 401
+    expect(last_response.body).not_to eq "{\"version\":\"#{Schleuder::VERSION}\"}"
+  end
+
+  it 'returns the current schleuder version if authorized as subscriber' do
+    subscription = create(:subscription, admin: false)
+    account = create(:account, email: subscription.email)
+    authorize!(account.email, account.set_new_password!)
+
+    get '/version.json'
+
+    expect(last_response.status).to be 200
+    expect(last_response.body).to eq "{\"version\":\"#{Schleuder::VERSION}\"}"
+  end
+
+  # The version is independent from any list, thus we skip the extra test for
+  # unassociated admin.
+
+  it 'returns the current schleuder version if authorized as list-admin' do
+    subscription = create(:subscription, admin: true)
+    account = create(:account, email: subscription.email)
+    authorize!(account.email, account.set_new_password!)
+
+    get '/version.json'
+
+    expect(last_response.status).to be 200
+    expect(last_response.body).to eq "{\"version\":\"#{Schleuder::VERSION}\"}"
+  end
+
+  it 'returns the current schleuder version if authorized as api_superadmin' do
+    authorize_as_api_superadmin!
+
     get '/version.json'
 
     expect(last_response.status).to be 200
