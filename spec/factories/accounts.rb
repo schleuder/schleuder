fc10b30abae7f506776beef7b5c0ec5f2bb73f69
Nina Siessegger <n.siessegger@zeromail.org>
2018-11-12 22:50:31 +0100

    Use dynamic attributes to fix FactoryBot deprecation warning

--- spec/factories/accounts.rb
+++ spec/factories/accounts.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :account do
     sequence(:email) {|n| "subscription#{n}@example.org" }
-    password SecureRandom.hex
+    password { SecureRandom.hex }
   end
 end
 
