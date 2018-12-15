88fb29a594d559c662800349f6ba0c6e95af33e7
paz <paz@nadir.org>
2018-12-15 14:20:55 +0100

    More unit specs around keyword handlers.

---
+++ spec/schleuder/unit/keyword_handlers/key_management_spec.rb
@@ -0,0 +1,170 @@
+require 'spec_helper'
+
+describe Schleuder::KeywordHandlers::KeyManagement do
+  it 'registers keywords' do
+    found_keywords = KeywordHandlersRunner::REGISTERED_KEYWORDS.values.map do |hash|
+      hash.select do |keyword, attributes|
+        attributes[:klass] == Schleuder::KeywordHandlers::KeyManagement
+      end.keys
+    end.flatten
+    
+    expect(found_keywords).to eql(%w[add-key delete-key list-keys list-key get-key get-keys fetch-key fetch-keys])
+  end
+
+  context '.add_key' do
+    it 'imports a key from inline material' do
+      mail = Mail.new
+      mail.list = create(:list)
+      mail.body = File.read('spec/fixtures/example_key.txt')
+      mail.to_s
+
+      list_keys = mail.list.keys
+      output = KeywordHandlers::KeyManagement.new(mail: mail, arguments: []).add_key
+
+      expect(output).to eql("This key was newly added:\n0xC4D60F8833789C7CAA44496FD3FFA6613AB10ECE schleuder2@example.org 2016-12-12\n")
+      expect(mail.list.keys.size).to eql(list_keys.size + 1)
+    end
+
+    it 'imports a key from attached material' do
+      mail = Mail.new
+      mail.list = create(:list)
+      mail.add_file('spec/fixtures/example_key.txt')
+      mail.to_s
+
+      list_keys = mail.list.keys
+      output = KeywordHandlers::KeyManagement.new(mail: mail, arguments: []).add_key
+
+      expect(output).to eql("This key was newly added:\n0xC4D60F8833789C7CAA44496FD3FFA6613AB10ECE schleuder2@example.org 2016-12-12\n")
+      expect(mail.list.keys.size).to eql(list_keys.size + 1)
+    end
+
+    it 'ignores body if an attachment is present' do
+      mail = Mail.new
+      mail.list = create(:list)
+      mail.body = 'blabla'
+      mail.add_file('spec/fixtures/example_key.txt')
+      mail.to_s
+
+      list_keys = mail.list.keys
+      output = KeywordHandlers::KeyManagement.new(mail: mail, arguments: []).add_key
+
+      expect(output).to eql("This key was newly added:\n0xC4D60F8833789C7CAA44496FD3FFA6613AB10ECE schleuder2@example.org 2016-12-12\n")
+      expect(mail.list.keys.size).to eql(list_keys.size + 1)
+    end
+
+    it 'ignores arguments' do
+      mail = Mail.new
+      mail.list = create(:list)
+      mail.body = 'blabla'
+      mail.to_s
+      list_keys = mail.list.keys
+      key_material = File.read('spec/fixtures/example_key.txt').lines
+
+      output = KeywordHandlers::KeyManagement.new(mail: mail, arguments: key_material).add_key
+
+      expect(output).to eql('In the message you sent us, no keys could be found. :(')
+      expect(mail.list.keys.size).to eql(list_keys.size)
+    end
+
+    it 'updates a key' do
+      mail = Mail.new
+      mail.list = create(:list)
+      mail.body = 'blabla'
+      mail.add_file('spec/fixtures/expired_key_extended.txt')
+      mail.to_s
+      mail.list.import_key(File.read('spec/fixtures/expired_key.txt'))
+      list_keys = mail.list.keys
+
+      output = KeywordHandlers::KeyManagement.new(mail: mail, arguments: []).add_key
+
+      expect(output).to eql("This key was updated:\n0x98769E8A1091F36BD88403ECF71A3F8412D83889 bla@foo 2010-08-13 [expired: 2017-01-20]\n")
+      expect(mail.list.keys.size).to eql(list_keys.size)
+    end
+
+    it 'rejects garbage' do
+      mail = Mail.new
+      mail.list = create(:list)
+      mail.body = 'blabla'
+      mail.to_s
+      list_keys = mail.list.keys
+
+      output = KeywordHandlers::KeyManagement.new(mail: mail, arguments: []).add_key
+
+      expect(output).to eql('In the message you sent us, no keys could be found. :(')
+      expect(mail.list.keys.size).to eql(list_keys.size)
+    end
+  end
+
+  context '.delete_key' do
+    it 'deletes a key that distinctly matches the argument' do
+      mail = Mail.new
+      mail.list = create(:list)
+      mail.list.import_key(File.read('spec/fixtures/example_key.txt'))
+      mail.body = 'ignore me'
+      mail.to_s
+      list_keys = mail.list.keys
+
+      output = KeywordHandlers::KeyManagement.new(mail: mail, arguments: ['C4D60F8833789C7CAA44496FD3FFA6613AB10ECE']).delete_key
+
+      expect(output).to eql("This key was deleted:\n0xC4D60F8833789C7CAA44496FD3FFA6613AB10ECE schleuder2@example.org 2016-12-12\n")
+      expect(mail.list.keys.size).to eql(list_keys.size - 1)
+    end
+
+    it 'deletes multiple keys that each distinctly match one argument' do
+      mail = Mail.new
+      mail.list = create(:list)
+      mail.list.import_key(File.read('spec/fixtures/example_key.txt'))
+      mail.list.import_key(File.read('spec/fixtures/expired_key_extended.txt'))
+      mail.body = 'ignore me'
+      mail.to_s
+      list_keys = mail.list.keys
+
+      output = KeywordHandlers::KeyManagement.new(mail: mail, arguments: ['C4D60F8833789C7CAA44496FD3FFA6613AB10ECE', '98769E8A1091F36BD88403ECF71A3F8412D83889']).delete_key
+
+      expect(output).to eql("This key was deleted:\n0xC4D60F8833789C7CAA44496FD3FFA6613AB10ECE schleuder2@example.org 2016-12-12\n\n\nThis key was deleted:\n0x98769E8A1091F36BD88403ECF71A3F8412D83889 bla@foo 2010-08-13 [expired: 2017-01-20]\n")
+      expect(mail.list.keys.size).to eql(list_keys.size - 2)
+    end
+
+    it 'deletes no key if the argument matches but not distinctly' do
+      mail = Mail.new
+      mail.list = create(:list)
+      mail.list.import_key(File.read('spec/fixtures/example_key.txt'))
+      mail.body = 'ignore me'
+      mail.to_s
+      list_keys = mail.list.keys
+
+      output = KeywordHandlers::KeyManagement.new(mail: mail, arguments: ['schleuder']).delete_key
+
+      expect(output).to eql("Too many matching keys for 'schleuder':\npub   4096R/59C71FB38AEE22E091C78259D06350440F759BD3 2016-12-06\nuid\t\tSchleuder TestUser <schleuder@example.org>\nsub   4096R/59C71FB38AEE22E091C78259D06350440F759BD3 2016-12-06\nsub   4096R/6B8F6C6384D2F9860A8F1E56AF755FC1A5D8C02B 2016-12-06\n\npub   4096R/C4D60F8833789C7CAA44496FD3FFA6613AB10ECE 2016-12-12\nuid\t\tAnother Testuser <schleuder2@example.org>\nsub   4096R/C4D60F8833789C7CAA44496FD3FFA6613AB10ECE 2016-12-12\nsub   4096R/3473864E7188C72EFF6246C2C38DE7E96D4EB747 2016-12-12\n\n")
+      expect(mail.list.keys.size).to eql(list_keys.size)
+    end
+
+    it 'deletes no key if the argument does not match' do
+      mail = Mail.new
+      mail.list = create(:list)
+      mail.list.import_key(File.read('spec/fixtures/example_key.txt'))
+      mail.body = 'ignore me'
+      mail.to_s
+      list_keys = mail.list.keys
+
+      output = KeywordHandlers::KeyManagement.new(mail: mail, arguments: ['0x0x0x']).delete_key
+
+      expect(output).to eql('No match found for 0x0x0x')
+      expect(mail.list.keys.size).to eql(list_keys.size)
+    end
+
+    it 'sends error message if no argument is given' do
+      mail = Mail.new
+      mail.list = create(:list)
+      mail.list.import_key(File.read('spec/fixtures/example_key.txt'))
+      mail.body = 'ignore me'
+      mail.to_s
+      list_keys = mail.list.keys
+
+      output = KeywordHandlers::KeyManagement.new(mail: mail, arguments: []).delete_key
+
+      expect(output).to eql("Error: You did not send any arguments for the keyword 'DELETE-KEY'.\n\nOne is required, more are optional, e.g.:\nX-DELETE-KEY: 0xB3D190D5235C74E1907EACFE898F2C91E2E6E1F3\n\nOr, to delete multiple keys at once:\nX-DELETE-KEY: 0xB3D190D5235C74E1907EACFE898F2C91E2E6E1F3 a-subscription@hostname\n\nThe matching keys will be deleted only if the argument matches them distinctly.\n")
+      expect(mail.list.keys.size).to eql(list_keys.size)
+    end
+  end
+end
---
+++ spec/schleuder/unit/keyword_handlers_runner_spec.rb
@@ -0,0 +1,96 @@
+require 'spec_helper'
+
+describe 'KeywordHandlersRunner' do
+  it 'stores a keyword that was registered' do
+    KeywordHandlersRunner.register_keyword(
+      type: :request,
+      keyword: 'a-keyword',
+      handler_class: Object,
+      handler_method: 'a_method',
+      aliases: 'an-alias'
+    )
+
+    expect(KeywordHandlersRunner::REGISTERED_KEYWORDS[:request]['a-keyword']).to be_a(Hash)
+    expect(KeywordHandlersRunner::REGISTERED_KEYWORDS[:request]['an-alias']).to eql(KeywordHandlersRunner::REGISTERED_KEYWORDS[:request]['a-keyword'])
+  end
+
+  it 'requires X-LIST-NAME' do
+    mail = Mail.new
+    mail.body = 'x-list-keys'
+    mail.to_s
+
+    output = KeywordHandlersRunner.run(mail: mail, list: create(:list), type: :request)
+
+    expect(output).to eql(['Your message did not contain the required "X-LIST-NAME" keyword and was rejected.'])
+  end
+
+  it 'rejects X-LIST-NAME with mismatching argument' do
+    mail = Mail.new
+    mail.body = 'x-list-name: something'
+    mail.to_s
+
+    output = KeywordHandlersRunner.run(mail: mail, list: create(:list), type: :request)
+
+    expect(output).to eql(['Your message contained an incorrect "X-LIST-NAME" keyword. The keyword argument must match the email address of this list.'])
+  end
+
+  it 'requires X-STOP' do
+    list = create(:list)
+    mail = Mail.new
+    mail.body = "x-list-keys\nx-list-name: #{list.email}"
+    mail.to_s
+
+    output = KeywordHandlersRunner.run(mail: mail, list: list, type: :request)
+
+    expect(output).to eql(["Your message lacked the keyword 'X-STOP'. If you use keywords in a message, you must indicate with 'X-STOP' where to stop looking for further keywords."])
+  end
+
+  it 'rejects unknown keywords' do
+    list = create(:list)
+    mail = Mail.new
+    mail.body = "x-list-subscriptions\nx-blabla\nx-list-name: #{list.email}\nx-stop"
+    mail.to_s
+
+    output = KeywordHandlersRunner.run(mail: mail, list: list, type: :request)
+
+    expect(output).to eql(["The given keyword 'blabla' is unknown. Please check its spelling or the documentation."])
+  end
+
+  it 'does not require mandatory keywords if no keywords are present' do
+    output = KeywordHandlersRunner.run(mail: Mail.new, list: create(:list), type: :request)
+    expect(output).to eql([])
+  end
+
+  it 'loads additional keyword handlers' do
+    list = create(:list)
+    mail = Mail.new
+    mail.body = "x-custom-keyword\nx-list-name: #{list.email}\nx-stop"
+    mail.to_s
+
+    output = KeywordHandlersRunner.run(mail: mail, list: list, type: :request)
+
+    expect(CustomKeyword.ancestors).to include(Schleuder::KeywordHandlers::Base)
+    expect(output).to eql(['Something something'])
+  end
+
+  it 'notifies admins' do
+    list = create(:list, keywords_admin_notify: ['list-subscriptions'])
+    list.subscribe('schleuder@example.org', '59C71FB38AEE22E091C78259D06350440F759BD3', true)
+    mail = Mail.new
+    mail.list = list
+    mail.body = "x-list-subscriptions\nx-list-name: #{list.email}\nx-stop"
+    mail.to_s
+
+    output = KeywordHandlersRunner.run(mail: mail, list: list, type: :request)
+    raw = Mail::TestMailer.deliveries.first
+    message = Mail.create_message_to_list(raw.to_s, list.request_address, list).setup
+    wanted_response = "Subscriptions:\n\nschleuder@example.org\t0x59C71FB38AEE22E091C78259D06350440F759BD3"
+
+    expect(output).to eql([wanted_response])
+    expect(Mail::TestMailer.deliveries.count).to be(1)
+    expect(message.to).to eql(['schleuder@example.org'])
+    expect(message.subject).to eql('Notice')
+    expect(message.first_plaintext_part.body.to_s).to include('list-subscriptions: ')
+    expect(message.first_plaintext_part.body.to_s).to include(wanted_response)
+  end
+end
