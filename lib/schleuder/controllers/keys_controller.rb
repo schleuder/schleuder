2c62e8164cbafb2a0e5778d7f49b8dc9475aac76
paz <paz@nadir.org>
2019-01-08 12:25:26 +0100

    Rename authorizing method in controllers.
    It does not return a boolean, which we would have to check, but it
    raises an exception, so it should end in an exclamation mark, not a
    question mark.
    
    (cherry picked from commit ccabd85dd3d2b5ec017d87ca5ee857647ce5dca3)

--- lib/schleuder/controllers/keys_controller.rb
+++ lib/schleuder/controllers/keys_controller.rb
@@ -2,33 +2,33 @@ module Schleuder
   class KeysController < BaseController
     def find_all(list_id)
       list = get_list_by_id_or_email(list_id)
-      authorized?(list, :list_keys)
+      authorize!(list, :list_keys)
       list.keys
     end
 
     def import(list_id, key)
       list = get_list_by_id_or_email(list_id)
-      authorized?(list, :add_keys)
+      authorize!(list, :add_keys)
       list.import_key(key)
     end
 
     def check(list_id)
       list = get_list_by_id_or_email(list_id)
-      authorized?(list, :check_keys)
+      authorize!(list, :check_keys)
       list.check_keys
     end
 
     def find(list_id, fingerprint)
       list = get_list_by_id_or_email(list_id)
       key = list.key(fingerprint)
-      authorized?(key, :read)
+      authorize!(key, :read)
       key
     end
 
     def delete(list_id, fingerprint)
       list = get_list_by_id_or_email(list_id)
       key = list.key(fingerprint) || halt(404)
-      authorized?(key, :delete)
+      authorize!(key, :delete)
       key.delete!
     end
   end
