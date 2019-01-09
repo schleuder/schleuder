732cb57a59c02cd19fb6f2a9a25b359ab8ad1e74
paz <paz@nadir.org>
2019-01-09 12:53:01 +0100

    Fix message for Errors::Unauthorized.
    (cherry picked from commit b3d8053236c120e25eec9b2ded75de89c7d22c94)

--- lib/schleuder/authorizer.rb
+++ lib/schleuder/authorizer.rb
@@ -13,7 +13,7 @@ module Schleuder
 
       action = action.to_s
       action << '?' unless action.last == '?'
-      policy(resource).public_send(action) or fail(Errors::Unauthorized.new)
+      policy(resource).public_send(action) or fail(Errors::Unauthorized.new(resource))
     end
 
     def scoped(klass)
