7fc5d655e8a965e00bbfec26061d62cc2480cc4b
paz <paz@nadir.org>
2019-01-09 12:53:01 +0100

    Fix message for Errors::Unauthorized.
    (cherry picked from commit b3d8053236c120e25eec9b2ded75de89c7d22c94)

--- lib/schleuder/errors/unauthorized.rb
+++ lib/schleuder/errors/unauthorized.rb
@@ -1,8 +1,18 @@
 module Schleuder
   module Errors
     class Unauthorized < Base
-      def initialize
-        super t('errors.unauthorized')
+      def initialize(resource=nil)
+        owner_email = case resource
+          when List
+            resource.owner_address
+          when Subscription, GPGME::Key
+            resource.list.owner_address
+          else
+            # Shouldn't happen, but who knows what life brings...
+            'LISTNAME-owner@DOMAIN'
+          end
+
+        super t('errors.unauthorized', list_owner_email: owner_email)
       end
     end
   end
