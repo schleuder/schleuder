8b319c83ad2d5641d1cc265ca2fd6c3cdc3f14ca
Nina Siessegger <n.siessegger@zeromail.org>
2019-01-17 18:55:44 +0100

    Remove unused methods from base controller
    (cherry picked from commit 51e35ffd4789a1bb94ae96e25aadbce0c1b51c72)

--- lib/schleuder/controllers/base_controller.rb
+++ lib/schleuder/controllers/base_controller.rb
@@ -15,17 +15,5 @@ module Schleuder
     def get_list(email)
       List.find_by(email: email.to_s)
     end
-
-    def to_query_args(identifier)
-      if is_an_integer?(identifier)
-        {id: identifier.to_i}
-      else
-        {email: identifier.to_s}
-      end
-    end
-
-    def is_an_integer?(input)
-      input.to_s.match(/^[0-9]+$/).present?
-    end
   end
 end
