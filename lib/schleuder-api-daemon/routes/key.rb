30db519b97ec1dbb76a9433c17e9115d1ad95323
Nina Siessegger <n.siessegger@zeromail.org>
2019-02-03 18:25:24 +0100

    Nest keys resource under /lists
    (cherry picked from commit 31cba4f1ceee3b36476216a7220726367bc5eed7)

--- lib/schleuder-api-daemon/routes/key.rb
+++ lib/schleuder-api-daemon/routes/key.rb
@@ -1,38 +1,34 @@
 class SchleuderApiDaemon < Sinatra::Base
   register Sinatra::Namespace
 
-  namespace '/keys' do
-    get '.json' do
-      require_list_email_param('list_email')
-      keys = keys_controller.find_all(params[:list_email])
+  namespace '/lists' do
+    get '/:list_email/keys.json' do |list_email|
+      keys = keys_controller.find_all(list_email)
       keys_hash = keys.sort_by(&:email).map do |key|
         key_to_hash(key)
       end
       json keys_hash
     end
 
-    post '.json' do
+    post '/:list_email/keys.json' do |list_email|
       input = parsed_body['keymaterial']
       if !input.match('BEGIN PGP')
         input = Base64.decode64(input)
       end
-      json keys_controller.import(requested_list_email, input)
+      json keys_controller.import(list_email, input)
     end
 
-    get '/check_keys.json' do
-      require_list_email_param('list_email')
-      json result: keys_controller.check(params[:list_email])
+    get '/:list_email/keys/check.json' do |list_email|
+      json result: keys_controller.check(list_email)
     end
 
-    get '/:fingerprint.json' do |fingerprint|
-      require_list_email_param('list_email')
-      key = keys_controller.find(params[:list_email], fingerprint)
+    get '/:list_email/keys/:fingerprint.json' do |list_email, fingerprint|
+      key = keys_controller.find(list_email, fingerprint)
       json key_to_hash(key, true)
     end
 
-    delete '/:fingerprint.json' do |fingerprint|
-      require_list_email_param('list_email')
-      keys_controller.delete(params[:list_email], fingerprint)
+    delete '/:list_email/keys/:fingerprint.json' do |list_email, fingerprint|
+      keys_controller.delete(list_email, fingerprint)
     end
   end
 
