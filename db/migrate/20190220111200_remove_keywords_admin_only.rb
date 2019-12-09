d189ca1915d9be902ddfa0c70b0ee55bf5a22a31
paz <paz@nadir.org>
2019-12-09 14:46:31 +0100

    Migrate data of list.keywords_admin_only to list.subscriber_permissions
    (cherry picked from commit 3df603e58a9e8d9679e4eb62664f237fce740ec6)

--- db/migrate/20190220111200_remove_keywords_admin_only.rb
+++
@@ -1,9 +0,0 @@
-class RemoveKeywordsAdminOnly < ActiveRecord::Migration[5.2]
-  def up
-    remove_column :lists, :keywords_admin_only
-  end
-
-  def down
-    add_column :lists, :keywords_admin_only, :text, default: "[\"subscribe\", \"unsubscribe\", \"delete-key\"]"
-  end
-end
