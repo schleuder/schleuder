c9744bb99ce917cf31b6f8732202654c90e8066f
Nina Siessegger <n.siessegger@zeromail.org>
2020-01-03 11:39:33 +0100

    Wrap all specs related to get request in context block

--- spec/schleuder-api-daemon/requests/list_spec.rb
+++ spec/schleuder-api-daemon/requests/list_spec.rb
@@ -72,67 +72,69 @@ describe 'lists via api' do
     end
   end
 
-  it "doesn't show a list without authentication" do
-    list = create(:list)
+  context 'get list' do
+    it "doesn't show a list without authentication" do
+      list = create(:list)
 
-    get "lists/#{list.email}.json"
+      get "lists/#{list.email}.json"
 
-    expect(last_response.status).to be 401
-    expect(last_response.body).to eql('Not authenticated')
-  end
+      expect(last_response.status).to be 401
+      expect(last_response.body).to eql('Not authenticated')
+    end
 
-  it "doesn't show a list authorized as unassociated account" do
-    list = create(:list)
-    subscription = create(:subscription, admin: true)
-    account = create(:account, email: subscription.email)
-    authorize!(account.email, account.set_new_password!)
+    it "doesn't show a list authorized as unassociated account" do
+      list = create(:list)
+      subscription = create(:subscription, admin: true)
+      account = create(:account, email: subscription.email)
+      authorize!(account.email, account.set_new_password!)
 
-    get "lists/#{list.email}.json"
+      get "lists/#{list.email}.json"
 
-    expect(last_response.status).to be 403
-    expect(last_response.body).to eql('Not authorized')
-  end
+      expect(last_response.status).to be 403
+      expect(last_response.body).to eql('Not authorized')
+    end
 
-  it "doesn't show a list authorized as subscriber" do
-    list = create(:list)
-    subscription = create(:subscription, list_id: list.id, admin: false)
-    account = create(:account, email: subscription.email)
-    authorize!(account.email, account.set_new_password!)
+    it "doesn't show a list authorized as subscriber" do
+      list = create(:list)
+      subscription = create(:subscription, list_id: list.id, admin: false)
+      account = create(:account, email: subscription.email)
+      authorize!(account.email, account.set_new_password!)
 
-    get "lists/#{list.email}.json"
+      get "lists/#{list.email}.json"
 
-    expect(last_response.status).to be 200
-    expect(JSON.parse(last_response.body)['email']).to eq list.email
-  end
+      expect(last_response.status).to be 200
+      expect(JSON.parse(last_response.body)['email']).to eq list.email
+    end
 
-  it 'does show a list authorized as list-admin' do
-    list = create(:list)
-    subscription = create(:subscription, list_id: list.id, admin: true)
-    account = create(:account, email: subscription.email)
-    authorize!(account.email, account.set_new_password!)
+    it 'does show a list authorized as list-admin' do
+      list = create(:list)
+      subscription = create(:subscription, list_id: list.id, admin: true)
+      account = create(:account, email: subscription.email)
+      authorize!(account.email, account.set_new_password!)
 
-    get "lists/#{list.email}.json"
+      get "lists/#{list.email}.json"
 
-    expect(last_response.status).to be 200
-    expect(JSON.parse(last_response.body)['email']).to eq list.email
-  end
+      expect(last_response.status).to be 200
+      expect(JSON.parse(last_response.body)['email']).to eq list.email
+    end
 
-  it 'does show a list authorized as api_superadmin' do
-    list = create(:list)
-    authorize_as_api_superadmin!
+    it 'does show a list authorized as api_superadmin' do
+      list = create(:list)
+      authorize_as_api_superadmin!
 
-    get "lists/#{list.email}.json"
+      get "lists/#{list.email}.json"
 
-    expect(last_response.status).to be 200
-    expect(JSON.parse(last_response.body)['email']).to eq list.email
-  end
+      expect(last_response.status).to be 200
+      expect(JSON.parse(last_response.body)['email']).to eq list.email
+    end
 
-  it 'correctly finds a list by email-address that starts with a number' do
-    authorize_as_api_superadmin!
-    list = create(:list, email: '9list@hostname')
-    get "lists/#{list.email}.json"
-    expect(last_response.status).to be 200
-    expect(JSON.parse(last_response.body)['email']).to eq list.email
+    it 'correctly finds a list by email-address that starts with a number' do
+      authorize_as_api_superadmin!
+      list = create(:list, email: '9list@hostname')
+      get "lists/#{list.email}.json"
+      expect(last_response.status).to be 200
+      expect(JSON.parse(last_response.body)['email']).to eq list.email
+    end
   end
 
   context 'configurable_attributes' do
