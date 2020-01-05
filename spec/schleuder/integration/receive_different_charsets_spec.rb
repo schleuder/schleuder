92c7ead414eba3b3787c597b95ff983e32142b56
ng <ng@immerda.ch>
2020-01-05 14:39:13 +0100

    fix #409 - default to ASCII-8BIT encoding for external data.
    this should ensure we are able to parse most incoming
    plain text mails in different charsets.
    
    See:
    
       * https://0xacab.org/schleuder/schleuder/issues/409
       * https://0xacab.org/schleuder/schleuder/merge_requests/301
       * https://ruby-doc.org/core-2.6.5/Encoding.html#class-Encoding-label-External+encoding
    
    Especially:
    
      * https://0xacab.org/schleuder/schleuder/issues/409#note_176612
      * https://idiosyncratic-ruby.com/56-us-ascii-8bit.html
      * https://github.com/mikel/mail/issues/1294#issuecomment-460855599

---
+++ spec/schleuder/integration/receive_different_charsets_spec.rb
@@ -0,0 +1,25 @@
+require "spec_helper"
+
+describe "user sends emails with different charsets" do
+  Dir["spec/fixtures/mails/charset_mails/*.eml"].each do |f|
+    it "works with #{File.basename(f,'.eml')}" do
+      start_smtp_daemon
+      list = create(:list)
+      list.subscribe("admin@example.org", nil, true)
+
+      # Clean any LANG from env as this is usually the case for MUAs
+      # https://0xacab.org/schleuder/schleuder/issues/409
+      with_env(ENV.delete_if {|key, value| key =~ /LANG/ || key =~ /LC/ }) do
+        error = run_schleuder(:work, list.email, f)
+        mails = Dir.glob("#{smtp_daemon_outputdir}/mail-*")
+
+        expect(error).to be_empty
+        expect(mails.size).to eq 1
+      end
+
+      stop_smtp_daemon
+    end
+  end
+end
+
+
