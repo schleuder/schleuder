20e2bb402133f9d3e8c7fdf38cd7f8370888e0f3
Nina Siessegger <n.siessegger@zeromail.org>
2020-04-14 17:55:24 +0200

    Remove configurable attributes node

--- lib/schleuder-api-daemon/routes/list.rb
+++ lib/schleuder-api-daemon/routes/list.rb
@@ -25,7 +25,7 @@ class SchleuderApiDaemon < Sinatra::Base
     end
 
     get '/configurable_attributes.json' do
-      json_body(configurable_attributes: lists_controller.get_configurable_attributes)
+      json_body(lists_controller.get_configurable_attributes)
     end
 
     post '/:list_email/send_list_key_to_subscriptions.json' do |list_email|
