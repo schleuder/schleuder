b968c2c078690ac4fa8d3536b5b500cbdf151660
paz <paz@nadir.org>
2020-10-13 21:33:31 +0200

    get-key in new scheme

---
+++ lib/schleuder/keyword_handlers/get_key.rb
@@ -0,0 +1,34 @@
+module Schleuder
+  module KeywordHandlers
+    class GetKey < Base
+      handles_request_keyword 'get-key'
+
+      WANTED_ARGUMENTS = [/\S+/]
+
+      def run(mail)
+        argument = @arguments.first
+        keys = keys_controller.find_all(@list.email, argument)
+
+        if keys.blank?
+          t('no_matching_keys', input: argument)
+        else
+          result = [t('matching_keys_intro', input: argument)]
+          keys.each do |key|
+            result << make_key_attachment(key)
+          end
+          result.flatten
+        end
+      end
+
+      private
+
+      def make_key_attachment(key)
+        attachment = Mail::Part.new
+        attachment.body = key.armored
+        attachment.content_type = 'application/pgp-keys'
+        attachment.content_disposition = "attachment; filename=#{key.fingerprint}.asc"
+        attachment
+      end
+    end
+  end
+end
