d7ba3944b15674dedfb139ad0b74ad92b1364dba
paz <paz@nadir.org>
2020-10-13 21:35:53 +0200

    fetch-key in new scheme

---
+++ lib/schleuder/keyword_handlers/fetch_key.rb
@@ -0,0 +1,18 @@
+module Schleuder
+  module KeywordHandlers
+    class FetchKey < Base
+      handles_request_keyword 'fetch-key'
+
+      WANTED_ARGUMENTS = [/\S+/]
+
+      def run(mail)
+        argument = @arguments.first
+        if argument.blank?
+          return t('fetch_key_requires_arguments')
+        end
+
+        keys_controller.fetch(@list.email, argument)
+      end
+    end
+  end
+end
