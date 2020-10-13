5424e62e06960d510d52a0a69e0a3249065e57e0
paz <paz@nadir.org>
2020-10-13 20:57:23 +0200

    attach-list-key in new scheme

--- lib/schleuder/keyword_handlers/attach_list_key.rb
+++ lib/schleuder/keyword_handlers/attach_list_key.rb
@@ -1,22 +1,25 @@
 module Schleuder
   module KeywordHandlers
     class AttachListKey < Base
-      handles_list_keyword 'attach-listkey', with_method: :attach_list_key
+      handles_list_keyword 'attach-listkey'
+      handles_list_keyword 'attach-list-key'
+
+      WANTED_ARGUMENTS = []
 
       # No need to authorize: there is no way to block a list from answering to
       # emails addressed to listname-sendkey@hostname, so we don't need a way
       # to block using this keyword.
-      def attach_list_key
-        filename = "#{@list.fingerprint}.pgpkey"
+      def run(mail)
+        filename = "#{mail.list.fingerprint}.pgpkey"
         # "Mail" only really converts to multipart if the content-type is blank.
-        @mail.content_type = nil
-        @mail.add_file({
+        mail.content_type = nil
+        mail.add_file({
           filename: filename,
-          content: @list.export_key
+          content: mail.list.export_key
         })
-        @mail.attachments[filename].content_type = 'application/pgp-keys'
-        @mail.attachments[filename].content_description = "OpenPGP public key of #{@list.email}"
-        @mail.attachments[filename].content_disposition = "attachment; filename=#{filename}"
+        mail.attachments[filename].content_type = 'application/pgp-keys'
+        mail.attachments[filename].content_description = "OpenPGP public key of #{mail.list.email}"
+        mail.attachments[filename].content_disposition = "attachment; filename=#{filename}"
         nil
       end
     end
