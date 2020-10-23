9d21273803a8fe395c8f79920775f6a48ab2d01d
paz <paz@nadir.org>
2020-10-23 23:43:59 +0200

    fixup new kwh

--- lib/schleuder/keyword_handlers/delete_key.rb
+++ lib/schleuder/keyword_handlers/delete_key.rb
@@ -1,23 +1,23 @@
 module Schleuder
   module KeywordHandlers
     class DeleteKey < Base
-      handles_request_keyword 'delete-key', with_arguments: [/\S+/]
+      handles_request_keyword 'delete-key', with_arguments: [/\S+/, Array.new(99, /(\S+)?/)].flatten
 
       def run
-        # TODO: Do we still need this check?
         if @arguments.blank?
           return t('delete_key_requires_arguments')
         end
 
-        argument = arguments.first
-        begin
-          key = keys_controller.delete(@list.email, argument)
-          t('deleted', key_string: key.summary)
-        rescue GPGME::Error::Conflict => exc
-          t('not_deletable', error: exc.message)
-        rescue Errors::KeyNotFound => exc
-          exc.to_s
-        end
+        @arguments.map do |argument|
+          begin
+            key = keys_controller.delete(@list.email, argument)
+            t('deleted', key_string: key.summary)
+          rescue GPGME::Error::Conflict => exc
+            t('not_deletable', error: exc.message)
+          rescue Errors::KeyNotFound => exc
+            exc.to_s
+          end
+        end.join("\n")
       end
     end
   end
