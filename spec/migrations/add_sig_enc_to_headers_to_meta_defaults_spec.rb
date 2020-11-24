960c768780b945efaf94330fb12c39ed34055f24
Nina Siessegger <n.siessegger@zeromail.org>
2020-11-24 10:09:46 +0100

    WIP: First attempt to fix one of the migration spec

--- spec/migrations/add_sig_enc_to_headers_to_meta_defaults_spec.rb
+++ spec/migrations/add_sig_enc_to_headers_to_meta_defaults_spec.rb
@@ -1,29 +1,31 @@
 require 'spec_helper'
 
 describe 'AddSigEncToHeadersToMetaDefaults' do
-  let(:migrations) { ActiveRecord::Migration[5.2].new.migration_context.migrations }
+  let(:newest_migration) { 20190906194820 }
   let(:migration_under_test) { 20180110203100 }
   let(:previous_migration) { 20170713215059 }
 
   after(:each) do
-    ActiveRecord::Migrator.new(:up, migrations).migrate
+    # How to run all migrations???
+    ActiveRecord::Base.connection.migration_context.run(:up, newest_migration)
+    # ActiveRecord::Migrator.new(:up, migrations).migrate
     List.reset_column_information
   end
 
   describe 'up' do
     it 'sets the column defaults' do
-      ActiveRecord::Migrator.new(:down, migrations, previous_migration).migrate
+      ActiveRecord::Base.connection.migration_context.run(:down, previous_migration)
       list_klass = create_list_klass
       list_klass.reset_column_information
 
-      ActiveRecord::Migrator.new(:up, migrations, migration_under_test).migrate
+      ActiveRecord::Base.connection.migration_context.run(:up, migration_under_test)
       list_klass.reset_column_information
 
       expect(list_klass.column_defaults['headers_to_meta']).to eql(['from', 'to', 'cc', 'date', 'sig', 'enc'])
     end
 
     it 'adds sig and enc to headers_to_meta for lists wihtout the attributes' do
-      ActiveRecord::Migrator.new(:down, migrations, previous_migration).migrate
+      ActiveRecord::Base.connection.migration_context.run(:down, previous_migration)
       list_klass = create_list_klass
       # We can not use the lists-factory and must skip the validations in order
       # to make this work with list-attributes that were added or changed after
@@ -32,7 +34,7 @@ describe 'AddSigEncToHeadersToMetaDefaults' do
       list.save(validate: false)
       expect(list.headers_to_meta).not_to include('enc', 'sig')
 
-      ActiveRecord::Migrator.new(:up, migrations, migration_under_test).migrate
+      ActiveRecord::Base.connection.migration_context.run(:up, migration_under_test)
       list_klass.reset_column_information
       list.reload
 
@@ -41,7 +43,7 @@ describe 'AddSigEncToHeadersToMetaDefaults' do
 
     it 'does not add sig and enc to headers to meta if the attributes already exist' do
       headers_to_meta_including_sig_and_enc = ['from', 'to', 'cc', 'date', 'sig', 'enc']
-      ActiveRecord::Migrator.new(:down, migrations, previous_migration).migrate
+      ActiveRecord::Base.connection.migration_context.run(:down, previous_migration)
       list_klass = create_list_klass
       # We can not use the lists-factory and must skip the validations in order
       # to make this work with list-attributes that were added or changed after
@@ -51,7 +53,7 @@ describe 'AddSigEncToHeadersToMetaDefaults' do
 
       expect(list.headers_to_meta).to eql headers_to_meta_including_sig_and_enc
 
-      ActiveRecord::Migrator.new(:up, migrations, migration_under_test).migrate
+      ActiveRecord::Base.connection.migration_context.run(:up, migration_under_test)
       list_klass.reset_column_information
       list.reload
 
@@ -61,18 +63,18 @@ describe 'AddSigEncToHeadersToMetaDefaults' do
 
   describe 'down' do
     it 'sets the column defaults' do
-      ActiveRecord::Migrator.new(:up, migrations, migration_under_test).migrate
+      ActiveRecord::Base.connection.migration_context.run(:up, migration_under_test)
       list_klass = create_list_klass
       list_klass.reset_column_information
 
-      ActiveRecord::Migrator.new(:down, migrations, previous_migration).migrate
+      ActiveRecord::Base.connection.migration_context.run(:down, previous_migration)
       list_klass.reset_column_information
 
       expect(list_klass.column_defaults['headers_to_meta']).to eql(['from', 'to', 'cc', 'date'])
     end
 
     it 'removes sig and enc from headers_to_meta from existing lists' do
-      ActiveRecord::Migrator.new(:up, migrations, migration_under_test).migrate
+      ActiveRecord::Base.connection.migration_context.run(:up, migration_under_test)
       list_klass = create_list_klass
       list_klass.reset_column_information
       # We can not use the lists-factory and must skip the validations in order
@@ -83,7 +85,7 @@ describe 'AddSigEncToHeadersToMetaDefaults' do
 
       expect(list.headers_to_meta).to include('enc', 'sig')
 
-      ActiveRecord::Migrator.new(:down, migrations, previous_migration).migrate
+      ActiveRecord::Base.connection.migration_context.run(:down, previous_migration)
       list_klass.reset_column_information
       list.reload
 
