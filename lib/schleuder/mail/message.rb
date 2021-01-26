e260826d700fda738cf3c0a9531db50633237bb7
pnd <pnd@systemli.org>
2021-01-26 21:03:19 +0100

    Add vendored bounce detection from bounce_email
    Included bounce detection from https://github.com/mailtop/bounce_email

--- lib/schleuder/mail/message.rb
+++ lib/schleuder/mail/message.rb
@@ -202,12 +202,24 @@ module Mail
     end
 
     def automated_message?
-      BounceEmail::Mail.new(original_message).bounced? ||
       @recipient.match(/-bounce@/).present? ||
           # Empty Return-Path
-          self.return_path.to_s == '<>'
+          self.return_path.to_s == '<>' ||
+          bounced?
     end
 
+    def bounced?
+      @bounced ||= detect_bounce(self) || (error_status != "unknown")
+    end
+
+    def diagnostic_code
+      @diagnostic_code ||= status_code_description(error_status)
+    end
+
+    def error_status
+      @error_status ||= detect_error_code(self)
+    end
+ 
     def keywords
       return @keywords if @keywords
 
@@ -517,6 +529,145 @@ module Mail
           "<#{string}>"
         end
       end.join(' ')
+    end
+
+    def detect_error_code(mail)
+      # Detects the error code of an email with different heuristics
+      # from: https://github.com/mailtop/bounce_email
+      
+      # Custom status codes
+      unicode_subject = mail.subject.to_s
+      unicode_subject = unicode_subject.encode('utf-8') if unicode_subject.respond_to?(:encode)
+ 
+      return '97' if unicode_subject.match(/delayed/i)
+      return '98' if unicode_subject.match(/(unzulässiger|unerlaubter) anhang/i)
+      return '99' if unicode_subject.match(/auto.*reply|férias|ferias|Estarei ausente|estou ausente|vacation|vocation|(out|away).*office|on holiday|abwesenheits|autorespond|Automatische|eingangsbestätigung/i)
+
+      # Feedback-Type: abuse
+      return '96' if mail.to_s.match(/Feedback-Type\: abuse/i)
+
+      if mail.parts[1]
+        match_parts = mail.parts[1].body.match(/(Status:.|550 |#)([245]\.[0-9]{1,3}\.[0-9]{1,3})/)
+        code = match_parts[2] if match_parts
+        return code if code
+      end
+
+      # Now try getting it from correct part of tmail
+      code = detect_bounce_status_code_from_text(mail.body)
+      return code if code
+
+      # OK getting desperate so try getting code from entire email
+      code = detect_bounce_status_code_from_text(mail.to_s)
+      code || 'unknown'
+    end
+
+    def detect_bounce(mail)
+      # Detects bounces from different parts of the email without error status codes
+      # from: https://github.com/mailtop/bounce_email
+      return true if mail.subject.to_s.match(/(returned|undelivered) mail|mail delivery( failed)?|(delivery )(status notification|failure)|failure notice|undeliver(able|ed)( mail)?|return(ing message|ed) to sender/i)
+      return true if mail.subject.to_s.match(/auto.*reply|vacation|vocation|(out|away).*office|on holiday|abwesenheits|autorespond|Automatische|eingangsbestätigung/i)
+      return true if mail['precedence'].to_s.match(/auto.*(reply|responder|antwort)/i)
+      return true if mail.from.to_s.match(/^(MAILER-DAEMON|POSTMASTER)\@/i)
+      false
+    end
+
+    def detect_bounce_status_code_from_text(text)
+      # Parses a text and uses pattern matching to determines its error status (RFC 3463)
+      # from: https://github.com/mailtop/bounce_email
+      return "5.0.0" if text.match(/Status: 5\.0\.0/i)
+      return "5.1.1" if text.match(/no such (address|user)|Recipient address rejected|User unknown|does not like recipient|The recipient was unavailable to take delivery of the message|Sorry, no mailbox here by that name|invalid address|unknown user|unknown local part|user not found|invalid recipient|failed after I sent the message|did not reach the following recipient|nicht zugestellt werden|o pode ser entregue para um ou mais/i)
+      return "5.1.2" if text.match(/unrouteable mail domain|Esta casilla ha expirado por falta de uso|I couldn't find any host named/i)
+      if text.match(/mailbox is full|Mailbox quota (usage|disk) exceeded|quota exceeded|Over quota|User mailbox exceeds allowed size|Message rejected\. Not enough storage space|user has exhausted allowed storage space|too many messages on the server|mailbox is over quota|mailbox exceeds allowed size|excedeu a quota/i)
+        return "5.2.2" if text.match(/This is a permanent error||(Status: |)5\.2\.2/i)
+        return "4.2.2"
+      end
+      return "5.1.0" if text.match(/Address rejected/)
+      return "4.1.2" if text.match(/I couldn't find any host by that name/)
+      return "4.2.0" if text.match(/not yet been delivered/i)
+      return "5.1.1" if text.match(/mailbox unavailable|No such mailbox|RecipientNotFound|not found by SMTP address lookup|Status: 5\.1\.1/i)
+      return "5.2.3" if text.match(/Status: 5\.2\.3/i) # Too messages in folder
+      return "5.4.0" if text.match(/Status: 5\.4\.0/i) # too many hops
+      return "5.4.4" if text.match(/Unrouteable address/i)
+      return "4.4.7" if text.match(/retry timeout exceeded/i)
+      return "5.2.0" if text.match(/The account or domain may not exist, they may be blacklisted, or missing the proper dns entries./i)
+      return "5.5.4" if text.match(/554 TRANSACTION FAILED/i)
+      return "4.4.1" if text.match(/Status: 4.4.1|delivery temporarily suspended|wasn't able to establish an SMTP connection/i)
+      return "5.5.0" if text.match(/550 OU\-002|Mail rejected by Windows Live Hotmail for policy reasons/i)
+      return "5.1.2" if text.match(/PERM_FAILURE: DNS Error: Domain name not found/i)
+      return "4.2.0" if text.match(/Delivery attempts will continue to be made for/i)
+      return "5.5.4" if text.match(/554 delivery error:/i)
+      return "5.1.1" if text.match(/550-5.1.1|This Gmail user does not exist/i)
+      return "5.7.1" if text.match(/5.7.1 Your message.*?was blocked by ROTA DNSBL/i) # AA added
+      return "5.7.2" if text.match(/not have permission to post messages to the group/i)
+      return "5.3.2" if text.match(/Technical details of permanent failure|Too many bad recipients/i) && (text.match(/The recipient server did not accept our requests to connect/i) || text.match(/Connection was dropped by remote host/i) || text.match(/Could not initiate SMTP conversation/i)) # AA added
+      return "4.3.2" if text.match(/Technical details of temporary failure/i) && (text.match(/The recipient server did not accept our requests to connect/i) || text.match(/Connection was dropped by remote host/i) || text.match(/Could not initiate SMTP conversation/i)) # AA added
+      return "5.0.0" if text.match(/Delivery to the following recipient failed permanently/i) # AA added
+      return '5.2.3' if text.match(/account closed|account has been disabled or discontinued|mailbox not found|prohibited by administrator|access denied|account does not exist/i)
+    end
+
+    def status_code_description(code)
+      # Get the status message to a status code
+      # from: https://github.com/mailtop/bounce_email
+      return 'unknown' if code.nil? or code == 'unknown'
+      reasons = {
+        '00' =>  "Other undefined status is the only undefined error code. It should be used for all errors for which only the class of the error is known.",
+        '10' =>  "Something about the address specified in the message caused this DSN.",
+        '11' =>  "The mailbox specified in the address does not exist.  For Internet mail names, this means the address portion to the left of the '@' sign is invalid.  This code is only useful for permanent failures.",
+        '12' =>  "The destination system specified in the address does not exist or is incapable of accepting mail.  For Internet mail names, this means the address portion to the right of the @ is invalid for mail.  This codes is only useful for permanent failures.",
+        '13' =>  "The destination address was syntactically invalid.  This can apply to any field in the address.  This code is only useful for permanent failures.",
+        '14' =>  "The mailbox address as specified matches one or more recipients on the destination system.  This may result if a heuristic address mapping algorithm is used to map the specified address to a local mailbox name.",
+        '15' =>  "This mailbox address as specified was valid.  This status code should be used for positive delivery reports.",
+        '16' =>  "The mailbox address provided was at one time valid, but mail is no longer being accepted for that address.  This code is only useful for permanent failures.",
+        '17' =>  "The sender's address was syntactically invalid.  This can apply to any field in the address.",
+        '18' =>  "The sender's system specified in the address does not exist or is incapable of accepting return mail.  For domain names, this means the address portion to the right of the @ is invalid for mail. ",
+        '20' =>  "The mailbox exists, but something about the destination mailbox has caused the sending of this DSN.",
+        '21' =>  "The mailbox exists, but is not accepting messages.  This may be a permanent error if the mailbox will never be re-enabled or a transient error if the mailbox is only temporarily disabled.",
+        '22' =>  "The mailbox is full because the user has exceeded a per-mailbox administrative quota or physical capacity.  The general semantics implies that the recipient can delete messages to make more space available.  This code should be used as a persistent transient failure.",
+        '23' =>  "A per-mailbox administrative message length limit has been exceeded.  This status code should be used when the per-mailbox message length limit is less than the general system limit.  This code should be used as a permanent failure.",
+        '24' =>  "The mailbox is a mailing list address and the mailing list was unable to be expanded.  This code may represent a permanent failure or a persistent transient failure. ",
+        '30' =>  "The destination system exists and normally accepts mail, but something about the system has caused the generation of this DSN.",
+        '31' =>  "Mail system storage has been exceeded.  The general semantics imply that the individual recipient may not be able to delete material to make room for additional messages.  This is useful only as a persistent transient error.",
+        '32' =>  "The host on which the mailbox is resident is not accepting messages.  Examples of such conditions include an immanent shutdown, excessive load, or system maintenance.  This is useful for both permanent and permanent transient errors. ",
+        '33' =>  "Selected features specified for the message are not supported by the destination system.  This can occur in gateways when features from one domain cannot be mapped onto the supported feature in another.",
+        '34' =>  "The message is larger than per-message size limit.  This limit may either be for physical or administrative reasons. This is useful only as a permanent error.",
+        '35' =>  "The system is not configured in a manner which will permit it to accept this message.",
+        '40' =>  "Something went wrong with the networking, but it is not clear what the problem is, or the problem cannot be well expressed with any of the other provided detail codes.",
+        '41' =>  "The outbound connection attempt was not answered, either because the remote system was busy, or otherwise unable to take a call.  This is useful only as a persistent transient error.",
+        '42' =>  "The outbound connection was established, but was otherwise unable to complete the message transaction, either because of time-out, or inadequate connection quality. This is useful only as a persistent transient error.",
+        '43' =>  "The network system was unable to forward the message, because a directory server was unavailable.  This is useful only as a persistent transient error. The inability to connect to an Internet DNS server is one example of the directory server failure error. ",
+        '44' =>  "The mail system was unable to determine the next hop for the message because the necessary routing information was unavailable from the directory server. This is useful for both permanent and persistent transient errors.  A DNS lookup returning only an SOA (Start of Administration) record for a domain name is one example of the unable to route error.",
+        '45' =>  "The mail system was unable to deliver the message because the mail system was congested. This is useful only as a persistent transient error.",
+        '46' =>  "A routing loop caused the message to be forwarded too many times, either because of incorrect routing tables or a user forwarding loop. This is useful only as a persistent transient error.",
+        '47' =>  "The message was considered too old by the rejecting system, either because it remained on that host too long or because the time-to-live value specified by the sender of the message was exceeded. If possible, the code for the actual problem found when delivery was attempted should be returned rather than this code.  This is useful only as a persistent transient error.",
+        '50' =>  "Something was wrong with the protocol necessary to deliver the message to the next hop and the problem cannot be well expressed with any of the other provided detail codes.",
+        '51' =>  "A mail transaction protocol command was issued which was either out of sequence or unsupported.  This is useful only as a permanent error.",
+        '52' =>  "A mail transaction protocol command was issued which could not be interpreted, either because the syntax was wrong or the command is unrecognized. This is useful only as a permanent error.",
+        '53' =>  "More recipients were specified for the message than could have been delivered by the protocol.  This error should normally result in the segmentation of the message into two, the remainder of the recipients to be delivered on a subsequent delivery attempt.  It is included in this list in the event that such segmentation is not possible.",
+        '54' =>  "A valid mail transaction protocol command was issued with invalid arguments, either because the arguments were out of range or represented unrecognized features. This is useful only as a permanent error. ",
+        '55' =>  "A protocol version mis-match existed which could not be automatically resolved by the communicating parties.",
+        '60' =>  "Something about the content of a message caused it to be considered undeliverable and the problem cannot be well expressed with any of the other provided detail codes. ",
+        '61' =>  "The media of the message is not supported by either the delivery protocol or the next system in the forwarding path. This is useful only as a permanent error.",
+        '62' =>  "The content of the message must be converted before it can be delivered and such conversion is not permitted.  Such prohibitions may be the expression of the sender in the message itself or the policy of the sending host.",
+        '63' =>  "The message content must be converted to be forwarded but such conversion is not possible or is not practical by a host in the forwarding path.  This condition may result when an ESMTP gateway supports 8bit transport but is not able to downgrade the message to 7 bit as required for the next hop.",
+        '64' =>  "This is a warning sent to the sender when message delivery was successfully but when the delivery required a conversion in which some data was lost.  This may also be a permanant error if the sender has indicated that conversion with loss is prohibited for the message.",
+        '65' =>  "A conversion was required but was unsuccessful.  This may be useful as a permanent or persistent temporary notification.",
+        '70' =>  "Something related to security caused the message to be returned, and the problem cannot be well expressed with any of the other provided detail codes.  This status code may also be used when the condition cannot be further described because of security policies in force.",
+        '71' =>  "The sender is not authorized to send to the destination. This can be the result of per-host or per-recipient filtering.  This memo does not discuss the merits of any such filtering, but provides a mechanism to report such. This is useful only as a permanent error.",
+        '72' =>  "The sender is not authorized to send a message to the intended mailing list. This is useful only as a permanent error.",
+        '73' =>  "A conversion from one secure messaging protocol to another was required for delivery and such conversion was not possible. This is useful only as a permanent error. ",
+        '74' =>  "A message contained security features such as secure authentication which could not be supported on the delivery protocol. This is useful only as a permanent error.",
+        '75' =>  "A transport system otherwise authorized to validate or decrypt a message in transport was unable to do so because necessary information such as key was not available or such information was invalid.",
+        '76' =>  "A transport system otherwise authorized to validate or decrypt a message was unable to do so because the necessary algorithm was not supported. ",
+        '77' =>  "A transport system otherwise authorized to validate a message was unable to do so because the message was corrupted or altered.  This may be useful as a permanent, transient persistent, or successful delivery code.",
+        #custom codes,
+        '96' =>  "Feedback Loop",
+        '97' =>  "Delayed",
+        '98' =>  "Not allowed Attachment",
+        '99' =>  "Vacation auto-reply",
+      }
+
+      code = code.gsub(/\./,'')[1..2]
+      reasons[code] || 'unknown'
     end
   end
 end
