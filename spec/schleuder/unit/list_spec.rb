d6f9c3909bf97b59a0187c10d0af86fb5e1d3710
paz <paz@schleuder.org>
2021-06-09 09:03:32 +0200

    Remove checking if gpg knows import-filter
    We require a version of gpg that does know about import-filters since
    v4.0. So this code is superfluous.

--- spec/schleuder/unit/list_spec.rb
+++ spec/schleuder/unit/list_spec.rb
@@ -551,7 +551,7 @@ describe Schleuder::List do
       teardown_list_and_mailer(list)
     end
 
-    it 'does not import non-self-signatures if gpg >= 2.1.15; or else sends a warning' do
+    it 'does not import non-self-signatures' do
       list = create(:list)
       list.delete_key('87E65ED2081AE3D16BE4F0A5EBDBE899251F2412')
       list.subscribe('admin@example.org', nil, true)
@@ -565,14 +565,7 @@ describe Schleuder::List do
       signature_output = list.gpg.class.gpgcli(['--list-sigs', '87E65ED2081AE3D16BE4F0A5EBDBE899251F2412'])[1].grep(/0F759BD3.*schleuder@example.org/)
 
       expect(output).to include("This key was fetched (new key):\n0x87E65ED2081AE3D16BE4F0A5EBDBE899251F2412 bla@foo")
-      if GPGME::Ctx.gpg_knows_import_filter?
-        expect(signature_output).to be_empty
-      else
-        message = Mail::TestMailer.deliveries.first
-        expect(message.to).to eql([Conf.superadmin])
-        expect(message.subject).to eql('Schleuder installation problem')
-        expect(signature_output).not_to be_empty
-      end
+      expect(signature_output).to be_empty
 
       teardown_list_and_mailer(list)
     end
