3a51e166c60f4e7a2af2c1630100037248dd7d69
paz <paz@schleuder.org>
2023-11-06 15:07:22 +0100

    API: Return key details in response to POST(key)
    This allows the frontends to show if a newly added key has technical
    problems, e.g. being expired.

--- lib/schleuder-api-daemon/routes/key.rb
+++ lib/schleuder-api-daemon/routes/key.rb
@@ -14,7 +14,28 @@ class SchleuderApiDaemon < Sinatra::Base
       if ! input.match('BEGIN PGP')
         input = Base64.decode64(input)
       end
-      json list(requested_list_id).import_key(input)
+      @list = list(requested_list_id)
+      import_result = @list.import_key(input)
+      keys = []
+      messages = []
+      import_result.imports.each do |import_status|
+        if import_status.action == 'error'
+          messages << "The key with the fingerprint #{import_status.fingerprint} could not be imported for unknown reasons"
+        else
+          key = @list.gpg.find_distinct_key(import_status.fingerprint)
+          if key
+            keys << key_to_hash(key).merge({import_action: import_status.action})
+          end
+        end
+      end
+      set_x_messages(messages)
+      # Use a Hash as single response object to stay more REST-like. (also,
+      # ActiveResource chokes if we return an array here).
+      # The 'type' attribute is only necessary to keep ActiveResource from
+      # complaining about "expected attributes to be able to convert to Hash",
+      # which for some reason is raised without it (or some other, similar
+      # attribute).
+      json({type: 'keys', keys: keys})
     end
 
     get '/check_keys.json' do
