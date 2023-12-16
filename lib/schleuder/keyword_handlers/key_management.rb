b2a4a3671227bbd71fc8e779d5e78eb5ca8b1065
paz <paz@schleuder.org>
2023-12-16 17:10:28 +0100

    add-key: Fix returning error message if email contained no content

--- lib/schleuder/keyword_handlers/key_management.rb
+++ lib/schleuder/keyword_handlers/key_management.rb
@@ -15,7 +15,7 @@ module Schleuder
             import_key_from_body
           else
             @list.logger.debug 'Found no attachments and an empty body - sending error message'
-            I18n.t('keyword_handlers.key_management.no_content_found')
+            return I18n.t('keyword_handlers.key_management.no_content_found')
           end
 
         import_stati = results.compact.collect(&:imports).flatten
